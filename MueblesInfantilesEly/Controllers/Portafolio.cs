7aecd976f323217fb356d4d75aab70ad2cf89e62
bastianjara <bastian.jara.cumian@gmail.com>
2020-09-30 01:06:44 -0300

    primer commit

---
+++ MueblesInfantilesEly/Controllers/Portafolio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MueblesInfantilesEly.Controllers
+{
+    public class Portafolio : Controller
+    {
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+        public IActionResult Comodas_y_Mudadores()
+        {
+            return View();
+        }
+        public IActionResult Cunas_y_Camas()
+        {
+            return View();
+        }
+        public IActionResult Organizadores()
+        {
+            return View();
+        }
+        public IActionResult Roperos_Completos_y_Dos_puertas()
+        {
+            return View();
+        }
+    }
+}
